205b128c1d5fd5fbccc8a7821656a656641fce5e
Edmond Chuc <edmond@kurrawong.ai>
2024-03-18 21:29:09 +1000

    Add conditional when GSQ_DATA_GIT_COMMIT env var is set

--- data_loading/entrypoint.sh
+++ data_loading/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 git clone --depth 1 https://github.com/geological-survey-of-queensland/vocabularies.git
+if [ -n "$GSQ_DATA_GIT_COMMIT" ]; then
+  git reset --hard "$GSQ_DATA_GIT_COMMIT"
+fi
 git clone --depth 1 --branch v1.0 https://github.com/geological-survey-of-queensland/qldgeofeatures-dataset.git
 
 echo "Creating Fuseki datasets..."
